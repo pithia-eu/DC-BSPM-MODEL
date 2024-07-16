bbb11eb7d76a014701bb88f521933c1ea3c4be8f
Dimitris Kagialis <kagialisd@gmail.com>
2024-07-16 13:29:45 +0300

    models.pithia.eu

--- API/README.md
+++
@@ -1,1 +0,0 @@
-nohup /home/ubuntu/.pyenv/versions/3.9.0/bin/python3.9 /home/ubuntu/.pyenv/versions/3.9.0/bin/uvicorn apis:app --host 0.0.0.0 --port 8080 --reload >> ./apis.log
--- start-api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Define the full path to the uvicorn executable
-uvicorn_executable="/home/ubuntu/.pyenv/shims/uvicorn"
-cd /home/ubuntu/plasmasphere/DC-BPIM/API
-"$uvicorn_executable" apis:app --reload --port 8080 --host 0.0.0.0
